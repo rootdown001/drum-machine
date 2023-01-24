b208712fcdcf9bd7b03d041dac0393f015167eab
Lance Anderson <lance.a.585@gmail.com>
2023-01-23 19:35:31 -0800

    Edit - Edit handleKeyDown to make work

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,35 @@
+import React, { useState } from 'react';
 import '../css/App.css';
 import DrumMachine from './DrumMachine';
 
 
 function App() {
+
+const [clipQ, setClipQ] = useState("https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3")
+const [clipW, setClipW] = useState("https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3")
+const [clipE, setClipE] = useState("https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3")
+const [clipA, setClipA] = useState("https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3")
+const [clipS, setClipS] = useState("https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3")
+const [clipD, setClipD] = useState("https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3")
+const [clipZ, setClipZ] = useState("https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3")
+const [clipX, setClipX] = useState("https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3")
+const [clipC, setClipC] = useState("https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3")
+
+
+
   return (
     <div className="App">
-      <DrumMachine />
+      <DrumMachine 
+        clipQ={clipQ}
+        clipW={clipW}
+        clipE={clipE}
+        clipA={clipA}
+        clipS={clipS}
+        clipD={clipD}
+        clipZ={clipZ}
+        clipX={clipX}
+        clipC={clipC}
+      />
     </div>
   );
 }
--- src/components/DrumMachine.js
+++ src/components/DrumMachine.js
@@ -1,13 +1,72 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import DrumPad from './DrumPad';
 
-export default function DrumMachine() {
+export default function DrumMachine(props) {
+
+const {
+    clipQ,
+    clipW,
+    clipE,
+    clipA,
+    clipS,
+    clipD,
+    clipZ,
+    clipX,
+    clipC
+} = props
+
 
     function handleAudioClick(e) {
         const audio = e.target.querySelector('.clip');
         audio.play();
 
     }
+
+    useEffect(() => {
+
+        function handleKeyDown(e) {
+
+            switch(e.key.toUpperCase()) {
+                case 'Q':
+                    document.querySelector("#Q").play();
+                    break;
+                case 'W':
+                    document.querySelector("#W").play();
+                    break;
+                case 'E':
+                    document.querySelector("#E").play();
+                    break;
+                case 'A':
+                    document.querySelector("#A").play();
+                    break;
+                case 'S':
+                    document.querySelector("#S").play();
+                    break;
+                case 'D':
+                    document.querySelector("#D").play();
+                    break;
+                case 'Z':
+                    document.querySelector("#Z").play();
+                    break;
+                case 'X':
+                    document.querySelector("#X").play();
+                    break;
+                case 'C':
+                    document.querySelector("#C").play();
+                    break;
+                default:
+                    break;
+            }
+        }
+        console.log('added')
+        document.addEventListener('keydown', handleKeyDown);
+
+        return () => {
+            console.log('removed')
+        document.removeEventListener('keydown', handleKeyDown);
+    };
+    })
+
 
   return (
     <>
@@ -16,23 +75,24 @@ export default function DrumMachine() {
         <div id="display">
             Display
             <table>
+                <tbody>
                 <tr>
                     <td>
                         <button className="drum-pad" id="heater-1" onClick={handleAudioClick}>
                             Q
-                            <audio className="clip" id="Q" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"></audio>
+                            <audio className="clip" id="Q" src={clipQ}></audio>
                         </button>
                     </td>
                     <td>
                         <button className="drum-pad" id="heater-2" onClick={handleAudioClick}>
                             W
-                            <audio className="clip" id="W" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"></audio>
+                            <audio className="clip" id="W" src={clipW}></audio>
                         </button>
                     </td>
                     <td>
                         <button className="drum-pad" id="heater-3" onClick={handleAudioClick}>
                             E
-                            <audio className="clip" id="E" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"></audio>
+                            <audio className="clip" id="E" src={clipE}></audio>
                         </button>
                     </td>
                 </tr>
@@ -40,19 +100,19 @@ export default function DrumMachine() {
                     <td>
                         <button className="drum-pad" id="heater-4" onClick={handleAudioClick}>
                             A
-                            <audio className="clip" id="A" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"></audio>
+                            <audio className="clip" id="A" src={clipA}></audio>
                         </button>
                     </td>
                     <td>
                         <button className="drum-pad" id="clap" onClick={handleAudioClick}>
                             S
-                            <audio className="clip" id="S" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"></audio>
+                            <audio className="clip" id="S" src={clipS}></audio>
                         </button>
                     </td>
                     <td>
                         <button className="drum-pad" id="open-hh" onClick={handleAudioClick}>
                             D
-                            <audio className="clip" id="D" src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"></audio>
+                            <audio className="clip" id="D" src={clipD}></audio>
                         </button>
                     </td>
                 </tr>
@@ -60,22 +120,23 @@ export default function DrumMachine() {
                     <td>
                         <button className="drum-pad" id="kick-n-hat" onClick={handleAudioClick}>
                             Z
-                            <audio className="clip" id="Z" src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"></audio>
+                            <audio className="clip" id="Z" src={clipZ}></audio>
                         </button>
                     </td>
                     <td>
                         <button className="drum-pad" id="kick" onClick={handleAudioClick}>
                             X
-                            <audio className="clip" id="X" src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"></audio>
+                            <audio className="clip" id="X" src={clipX}></audio>
                         </button>
                     </td>
                     <td>
                         <button className="drum-pad" id="closed-hh" onClick={handleAudioClick}>
                             C
-                            <audio className="clip" id="C" src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"></audio>
+                            <audio className="clip" id="C" src={clipC}></audio>
                         </button>
                     </td>
                 </tr>
+                </tbody>
             </table>
         </div>
     </div>
